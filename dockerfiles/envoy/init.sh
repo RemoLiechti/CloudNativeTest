3c3cd3416c11d60ad4c6ae2421aaeb2b6685b242
rliechti <rliechti@adobe.com>
2024-01-31 14:55:23 +0100

    init

--- dockerfiles/envoy/init.sh
+++ dockerfiles/envoy/init.sh
@@ -1,3 +1,3 @@
 export PROJECT_NAME=$(basename /workspaces/*)
 cp /var/lib/envoy/launch.json /workspaces/$PROJECT_NAME
-sed -i 's/_PROJECT_NAME_/$PROJECT_NAME/' /workspaces/$PROJECT_NAME/launch.json
+sed -i "s/_PROJECT_NAME_/$PROJECT_NAME/" /workspaces/$PROJECT_NAME/launch.json
