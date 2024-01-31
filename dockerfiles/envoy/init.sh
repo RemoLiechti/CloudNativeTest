ec789792817c7635208134580523d7564ce63368
rliechti <rliechti@adobe.com>
2024-01-31 15:10:19 +0100

    relocate launch json

--- dockerfiles/envoy/init.sh
+++ dockerfiles/envoy/init.sh
@@ -1,3 +1,4 @@
 export PROJECT_NAME=$(basename /workspaces/*)
-cp /var/lib/envoy/launch.json /workspaces/$PROJECT_NAME
-sed -i "s/_PROJECT_NAME_/$PROJECT_NAME/" /workspaces/$PROJECT_NAME/launch.json
+mkdir -p /workspaces/$PROJECT_NAME/.vscode
+cp /var/lib/envoy/launch.json /workspaces/$PROJECT_NAME/.vscode
+sed -i "s/_PROJECT_NAME_/$PROJECT_NAME/" /workspaces/$PROJECT_NAME/.vscode/launch.json
